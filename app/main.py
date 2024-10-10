daae5c5538314efc6b636a473149142bc7dcc5f9
ch999999 <izakchordiscord@gmail.com>
2024-10-10 14:58:03 +0800

    remove unnecessary endpoint

--- app/main.py
+++ app/main.py
@@ -18,9 +18,3 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
