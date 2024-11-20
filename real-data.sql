0e3a11bfda5b88f6db72c895c69b2dd8c3633a92
ch999999 <izakchordiscord@gmail.com>
2024-11-20 09:59:10 +0800

    can deploy to gcloud

--- real-data.sql
+++ real-data.sql
@@ -29,21 +29,21 @@ ORDER BY hall.id, row_number, seat_number;
 
 --Insert movies
 INSERT INTO "Movies" (id, title, genre, duration, release_date, last_showing_date, image_url, trailer_url, synopsis) VALUES
-('3d1fd545-0db9-4f54-914b-911e962d34b0', 'Red One', 'action', 120, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/red-one.jpg', '', 'A holiday-themed action-adventure involving a thrilling quest.'),
-('3bbb4a0d-c774-443a-b44b-0ad7695a658b', 'Death Whisper 2', 'horror', 95, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/death-whisperer-2.jpg', '', 'A sequel to Death Whisper, following teens uncovering more mysteries and horrors.'),
-('b91777df-7050-40a6-b5d2-1549b34e20ff', 'Don Dukun', 'comedy', 100, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/don-dukun.jpg', '', 'After finding a mysterious artifact, a man finds himself possessed by a centuries-old spirit.'),
-('d349bc89-793e-4bed-a684-3bbc97162d51', 'Venom: The Last Dance', 'action', 110, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/venom-the-last-dance.jpg', '', 'The final chapter of the journey of Eddie Brock as Venom, facing his ultimate nemesis.'),
-('8312a1c5-f285-4602-b9ef-2ce88aa79062', 'Smile 2', 'horror', 98, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/smile-2.jpg', '', 'A continuation of the terrifying story that explores the spread of supernatural horrors.'),
-('e33a67d8-eb0d-46f7-b895-3688679910ad', 'Moana 2', 'animation', 90, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/moana-2.jpg', '', 'Moana embarks on a new journey across the seas to save her island once again.'),
-('d6949d54-041e-4972-9348-28b52f49cc4b', 'Mufasa: The Lion King', 'animation', 85, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/mufasa-the-lion-king.jpg', '', 'The origin story of Mufasa, exploring his rise to power.'),
-('19eae21d-cd9e-4cf7-9269-8a1adc9b416d', 'Sonic 3', 'action', 105, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/sonic-the-hedgehog-3.jpg', '', 'Sonic, Tails, and Knuckles team up to face a new villain.'),
-('11429350-41f3-4b4a-ab7c-4dbcf6f3dbba', 'Dongeng Sang Kancil', 'adventure', 80, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/dongeng-sang-kancil.jpg', '', 'An animated retelling of the classic Malay folktales of Sang Kancil.'),
-('8b32f48d-6bc0-4891-a009-aeba59ee2c52', 'Kraven the Hunter', 'action', 130, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/kraven-the-hunter.jpg', '', 'A skilled hunter from Marvel comics, Kraven faces new challenges in the city.'),
-('c8c74caf-55f8-45a7-bbb3-d0cfa2fb6d10', 'Gladiator 2', 'action', 140, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/gladiator-2.jpg', '', 'The story follows a new warrior rising in Rome after the fall of Maximus.'),
-('347a0fc6-0541-44b4-aead-c34f7f783f10', 'Cesium Fallout', 'sci-fi', 115, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/cesium-fallout.jpg', '', 'After a nuclear accident, a scientist must save humanity from radioactive fallout.'),
-('68f0c881-371f-4a2d-8ea4-03b23004a7f0', 'Dear Santa', 'drama', 102, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/dear_santa.jpg', '', 'A young woman writes letters to Santa, only to get responses that change her life.'),
-('aba38539-abdb-4db6-818a-df092ffb8601', 'A Sudden Case of Christmas', 'romance', 88, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/sudden-case-of-christmas.jpg', '', 'A workaholic wakes up to find herself in a magical town that celebrates Christmas every day.'),
-('a616704c-9e54-47e3-a8ec-cb32398d1dbe', 'The Best Christmas Pageant Ever', 'comedy', 95, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/best-christmas-peagant-ever.jpg', '', 'Chaos ensues when a group of troublemakers joins the town Christmas pageant.');
+('3d1fd545-0db9-4f54-914b-911e962d34b0', 'Red One', 'action', 120, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/red-one.jpg', 'https://youtu.be/7l3hfD74X-4?si=g3LmIdAc--o1MapF', 'A holiday-themed action-adventure involving a thrilling quest.'),
+('3bbb4a0d-c774-443a-b44b-0ad7695a658b', 'Death Whisper 2', 'horror', 95, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/death-whisperer-2.jpg', 'https://youtu.be/nR_Xntr_XKo?si=5a_XQU8-bwcx_GW-', 'A sequel to Death Whisper, following teens uncovering more mysteries and horrors.'),
+('b91777df-7050-40a6-b5d2-1549b34e20ff', 'Don Dukun', 'comedy', 100, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/don-dukun.jpg', 'https://youtu.be/mirAy8FN6RY?si=Hwzsem-XEiMOxuEz', 'After finding a mysterious artifact, a man finds himself possessed by a centuries-old spirit.'),
+('d349bc89-793e-4bed-a684-3bbc97162d51', 'Venom: The Last Dance', 'action', 110, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/venom-the-last-dance.jpg', 'https://youtu.be/HyIyd9joTTc?si=6YMUWHNjJfjogkRn', 'The final chapter of the journey of Eddie Brock as Venom, facing his ultimate nemesis.'),
+('8312a1c5-f285-4602-b9ef-2ce88aa79062', 'Smile 2', 'horror', 98, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/smile-2.jpg', 'https://youtu.be/0HY6QFlBzUY?si=k3ZVoL2oU_1Dnxlf', 'A continuation of the terrifying story that explores the spread of supernatural horrors.'),
+('e33a67d8-eb0d-46f7-b895-3688679910ad', 'Moana 2', 'animation', 90, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/moana-2.jpg', 'https://youtu.be/JdSl4RMNtGE?si=5hHsVCfu5YQ_u4-O', 'Moana embarks on a new journey across the seas to save her island once again.'),
+('d6949d54-041e-4972-9348-28b52f49cc4b', 'Mufasa: The Lion King', 'animation', 85, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/mufasa-the-lion-king.jpg', 'https://youtu.be/lMXh6vjiZrI?si=F7pFSVVjO-GS_0Hl', 'The origin story of Mufasa, exploring his rise to power.'),
+('19eae21d-cd9e-4cf7-9269-8a1adc9b416d', 'Sonic 3', 'action', 105, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/sonic-the-hedgehog-3.jpg', 'https://youtu.be/qSu6i2iFMO0?si=y5c0QA8oI9N2fxEq', 'Sonic, Tails, and Knuckles team up to face a new villain.'),
+('11429350-41f3-4b4a-ab7c-4dbcf6f3dbba', 'Dongeng Sang Kancil', 'adventure', 80, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/dongeng-sang-kancil.jpg', 'https://youtu.be/ArQpTYHixl0?si=n-Cwjw_1sMx1bAgp', 'An animated retelling of the classic Malay folktales of Sang Kancil.'),
+('8b32f48d-6bc0-4891-a009-aeba59ee2c52', 'Kraven the Hunter', 'action', 130, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/kraven-the-hunter.jpg', 'https://youtu.be/hR1-ihzff3I?si=vNi0MZGFMha8834o', 'A skilled hunter from Marvel comics, Kraven faces new challenges in the city.'),
+('c8c74caf-55f8-45a7-bbb3-d0cfa2fb6d10', 'Gladiator 2', 'action', 140, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/gladiator-2.jpg', 'https://youtu.be/4rgYUipGJNo?si=ZNv01kcyEqBEjhwk', 'The story follows a new warrior rising in Rome after the fall of Maximus.'),
+('347a0fc6-0541-44b4-aead-c34f7f783f10', 'Cesium Fallout', 'sci-fi', 115, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/cesium-fallout.jpg', 'https://youtu.be/fUMNq662wWE?si=SZwpZrKvt44K5USl', 'After a nuclear accident, a scientist must save humanity from radioactive fallout.'),
+('68f0c881-371f-4a2d-8ea4-03b23004a7f0', 'Dear Santa', 'drama', 102, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/dear_santa.jpg', 'https://youtu.be/TuJ3mM001Os?si=whvwd2Ih8M7G5UAD', 'A young woman writes letters to Santa, only to get responses that change her life.'),
+('aba38539-abdb-4db6-818a-df092ffb8601', 'A Sudden Case of Christmas', 'romance', 88, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/sudden-case-of-christmas.jpg', 'https://youtu.be/KcTCsSUxjPM?si=Zp73D_-tCITFSynd', 'A workaholic wakes up to find herself in a magical town that celebrates Christmas every day.'),
+('a616704c-9e54-47e3-a8ec-cb32398d1dbe', 'The Best Christmas Pageant Ever', 'comedy', 95, '2024-10-01', '2024-12-31', 'https://ticket4u-bucket.s3.ap-southeast-1.amazonaws.com/best-christmas-peagant-ever.jpg', 'https://youtu.be/cT7x4HpCICc?si=aARmSL7mhBpJs0lQ', 'Chaos ensues when a group of troublemakers joins the town Christmas pageant.');
 
 
 INSERT INTO "Showings" (id, hall_id, movie_id, start_time, pricex100) VALUES
@@ -463,34 +463,34 @@ INSERT INTO "Showings" (id, hall_id, movie_id, start_time, pricex100) VALUES
 (gen_random_uuid(), '827465c3-a1fe-4245-b4bd-326b8371a502', '19eae21d-cd9e-4cf7-9269-8a1adc9b416d', '2024-12-09 11:00:00+00:00', 1800),
 (gen_random_uuid(), '827465c3-a1fe-4245-b4bd-326b8371a502', '347a0fc6-0541-44b4-aead-c34f7f783f10', '2024-12-09 14:00:00+00:00', 1800),
 
--- Hall C (Dec 9)
-(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', 'b91777df-7050-40a6-b5d2-1549b34e20ff', '2024-12-08 17:00:00+00:00', 1800),
-(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '8312a1c5-f285-4602-b9ef-2ce88aa79062', '2024-12-09 02:00:00+00:00', 1500),
-(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', 'c8c74caf-55f8-45a7-bbb3-d0cfa2fb6d10', '2024-12-09 05:00:00+00:00', 1500),
-(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '19eae21d-cd9e-4cf7-9269-8a1adc9b416d', '2024-12-09 08:00:00+00:00', 1800),
-(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '68f0c881-371f-4a2d-8ea4-03b23004a7f0', '2024-12-09 11:00:00+00:00', 1800),
-(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', 'aba38539-abdb-4db6-818a-df092ffb8601', '2024-12-09 14:00:00+00:00', 1800),
+-- Hall C (Dec 9)//all scetion wrong
+(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '68f0c881-371f-4a2d-8ea4-03b23004a7f0', '2024-12-08 17:00:00+00:00', 1800),
+(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', 'aba38539-abdb-4db6-818a-df092ffb8601', '2024-12-09 02:00:00+00:00', 1500),
+(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', 'd6949d54-041e-4972-9348-28b52f49cc4b', '2024-12-09 05:00:00+00:00', 1500),
+(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', 'd349bc89-793e-4bed-a684-3bbc97162d51', '2024-12-09 08:00:00+00:00', 1800),
+(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', 'a616704c-9e54-47e3-a8ec-cb32398d1dbe', '2024-12-09 11:00:00+00:00', 1800),
+(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '3d1fd545-0db9-4f54-914b-911e962d34b0', '2024-12-09 14:00:00+00:00', 1800),
 
--- Hall D (Dec 9)
-(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '8b32f48d-6bc0-4891-a009-aeba59ee2c52', '2024-12-08 17:00:00+00:00', 1800),
-(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '3bbb4a0d-c774-443a-b44b-0ad7695a658b', '2024-12-09 02:00:00+00:00', 1500),
-(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '11429350-41f3-4b4a-ab7c-4dbcf6f3dbba', '2024-12-09 05:00:00+00:00', 1500),
-(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', 'e33a67d8-eb0d-46f7-b895-3688679910ad', '2024-12-09 08:00:00+00:00', 1800),
-(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '347a0fc6-0541-44b4-aead-c34f7f783f10', '2024-12-09 11:00:00+00:00', 1800),
-(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', 'b91777df-7050-40a6-b5d2-1549b34e20ff', '2024-12-09 14:00:00+00:00', 1800),
+-- Hall D (Dec 9)//all section wrong
+(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '8312a1c5-f285-4602-b9ef-2ce88aa79062', '2024-12-08 17:00:00+00:00', 1800),
+(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', 'e33a67d8-eb0d-46f7-b895-3688679910ad', '2024-12-09 02:00:00+00:00', 1500),
+(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', 'a616704c-9e54-47e3-a8ec-cb32398d1dbe', '2024-12-09 05:00:00+00:00', 1500),
+(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '11429350-41f3-4b4a-ab7c-4dbcf6f3dbba', '2024-12-09 08:00:00+00:00', 1800),
+(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', 'b91777df-7050-40a6-b5d2-1549b34e20ff', '2024-12-09 11:00:00+00:00', 1800),
+(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '8b32f48d-6bc0-4891-a009-aeba59ee2c52', '2024-12-09 14:00:00+00:00', 1800),
 
 -- Hall E (Dec 9)
-(gen_random_uuid(), 'f826f9d7-a64b-4db5-ac9a-08e6e568fbe0', '11429350-41f3-4b4a-ab7c-4dbcf6f3dbba', '2024-12-08 17:00:00+00:00', 1800),
-(gen_random_uuid(), 'f826f9d7-a64b-4db5-ac9a-08e6e568fbe0', 'e33a67d8-eb0d-46f7-b895-3688679910ad', '2024-12-09 02:00:00+00:00', 1500),
-(gen_random_uuid(), 'f826f9d7-a64b-4db5-ac9a-08e6e568fbe0', '347a0fc6-0541-44b4-aead-c34f7f783f10', '2024-12-09 05:00:00+00:00', 1500),
-(gen_random_uuid(), 'f826f9d7-a64b-4db5-ac9a-08e6e568fbe0', 'b91777df-7050-40a6-b5d2-1549b34e20ff', '2024-12-09 08:00:00+00:00', 1800),
-(gen_random_uuid(), 'f826f9d7-a64b-4db5-ac9a-08e6e568fbe0', '8312a1c5-f285-4602-b9ef-2ce88aa79062', '2024-12-09 11:00:00+00:00', 1800),
+(gen_random_uuid(), 'f826f9d7-a64b-4db5-ac9a-08e6e568fbe0', 'a616704c-9e54-47e3-a8ec-cb32398d1dbe', '2024-12-08 17:00:00+00:00', 1800),
+(gen_random_uuid(), 'f826f9d7-a64b-4db5-ac9a-08e6e568fbe0', '11429350-41f3-4b4a-ab7c-4dbcf6f3dbba', '2024-12-09 02:00:00+00:00', 1500),
+(gen_random_uuid(), 'f826f9d7-a64b-4db5-ac9a-08e6e568fbe0', 'b91777df-7050-40a6-b5d2-1549b34e20ff', '2024-12-09 05:00:00+00:00', 1500),
+(gen_random_uuid(), 'f826f9d7-a64b-4db5-ac9a-08e6e568fbe0', '8b32f48d-6bc0-4891-a009-aeba59ee2c52', '2024-12-09 08:00:00+00:00', 1800),
+(gen_random_uuid(), 'f826f9d7-a64b-4db5-ac9a-08e6e568fbe0', '3d1fd545-0db9-4f54-914b-911e962d34b0', '2024-12-09 11:00:00+00:00', 1800),
 (gen_random_uuid(), 'f826f9d7-a64b-4db5-ac9a-08e6e568fbe0', 'c8c74caf-55f8-45a7-bbb3-d0cfa2fb6d10', '2024-12-09 14:00:00+00:00', 1800),
 
 -- Hall F (Dec 9)
-(gen_random_uuid(), 'b3ba36ff-39f1-4325-8a23-d93f09996e49', '347a0fc6-0541-44b4-aead-c34f7f783f10', '2024-12-08 17:00:00+00:00', 1800),
-(gen_random_uuid(), 'b3ba36ff-39f1-4325-8a23-d93f09996e49', 'b91777df-7050-40a6-b5d2-1549b34e20ff', '2024-12-09 02:00:00+00:00', 1500),
-(gen_random_uuid(), 'b3ba36ff-39f1-4325-8a23-d93f09996e49', '8312a1c5-f285-4602-b9ef-2ce88aa79062', '2024-12-09 05:00:00+00:00', 1500),
+(gen_random_uuid(), 'b3ba36ff-39f1-4325-8a23-d93f09996e49', 'b91777df-7050-40a6-b5d2-1549b34e20ff', '2024-12-08 17:00:00+00:00', 1800),
+(gen_random_uuid(), 'b3ba36ff-39f1-4325-8a23-d93f09996e49', '8b32f48d-6bc0-4891-a009-aeba59ee2c52', '2024-12-09 02:00:00+00:00', 1500),
+(gen_random_uuid(), 'b3ba36ff-39f1-4325-8a23-d93f09996e49', '3d1fd545-0db9-4f54-914b-911e962d34b0', '2024-12-09 05:00:00+00:00', 1500),
 (gen_random_uuid(), 'b3ba36ff-39f1-4325-8a23-d93f09996e49', 'c8c74caf-55f8-45a7-bbb3-d0cfa2fb6d10', '2024-12-09 08:00:00+00:00', 1800),
 (gen_random_uuid(), 'b3ba36ff-39f1-4325-8a23-d93f09996e49', '19eae21d-cd9e-4cf7-9269-8a1adc9b416d', '2024-12-09 11:00:00+00:00', 1800),
 (gen_random_uuid(), 'b3ba36ff-39f1-4325-8a23-d93f09996e49', '347a0fc6-0541-44b4-aead-c34f7f783f10', '2024-12-09 14:00:00+00:00', 1800);
@@ -514,20 +514,20 @@ INSERT INTO "Showings" (id, hall_id, movie_id, start_time, pricex100) VALUES
 (gen_random_uuid(), '827465c3-a1fe-4245-b4bd-326b8371a502', '8312a1c5-f285-4602-b9ef-2ce88aa79062', '2024-12-10 14:00:00+00:00', 1800),
 
 -- Hall C (Dec 10)
-(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '3d1fd545-0db9-4f54-914b-911e962d34b0', '2024-12-09 17:00:00+00:00', 1800),
-(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '8b32f48d-6bc0-4891-a009-aeba59ee2c52', '2024-12-10 02:00:00+00:00', 1500),
-(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '3bbb4a0d-c774-443a-b44b-0ad7695a658b', '2024-12-10 05:00:00+00:00', 1500),
-(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '11429350-41f3-4b4a-ab7c-4dbcf6f3dbba', '2024-12-10 08:00:00+00:00', 1800),
-(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', 'e33a67d8-eb0d-46f7-b895-3688679910ad', '2024-12-10 11:00:00+00:00', 1800),
-(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '347a0fc6-0541-44b4-aead-c34f7f783f10', '2024-12-10 14:00:00+00:00', 1800),
+(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '8b32f48d-6bc0-4891-a009-aeba59ee2c52', '2024-12-09 17:00:00+00:00', 1800),
+(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '3bbb4a0d-c774-443a-b44b-0ad7695a658b', '2024-12-10 02:00:00+00:00', 1500),
+(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '11429350-41f3-4b4a-ab7c-4dbcf6f3dbba', '2024-12-10 05:00:00+00:00', 1500),
+(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', 'e33a67d8-eb0d-46f7-b895-3688679910ad', '2024-12-10 08:00:00+00:00', 1800),
+(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', '347a0fc6-0541-44b4-aead-c34f7f783f10', '2024-12-10 11:00:00+00:00', 1800),
+(gen_random_uuid(), '5aa22443-5fc6-46a6-9fe6-0261c015251e', 'b91777df-7050-40a6-b5d2-1549b34e20ff', '2024-12-10 14:00:00+00:00', 1800),
 
 -- Hall D (Dec 10)
-(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '8312a1c5-f285-4602-b9ef-2ce88aa79062', '2024-12-09 17:00:00+00:00', 1800),
-(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '3bbb4a0d-c774-443a-b44b-0ad7695a658b', '2024-12-10 02:00:00+00:00', 1500),
-(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '11429350-41f3-4b4a-ab7c-4dbcf6f3dbba', '2024-12-10 05:00:00+00:00', 1500),
-(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', 'e33a67d8-eb0d-46f7-b895-3688679910ad', '2024-12-10 08:00:00+00:00', 1800),
-(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '347a0fc6-0541-44b4-aead-c34f7f783f10', '2024-12-10 11:00:00+00:00', 1800),
-(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', 'b91777df-7050-40a6-b5d2-1549b34e20ff', '2024-12-10 14:00:00+00:00', 1800),
+(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '3d1fd545-0db9-4f54-914b-911e962d34b0', '2024-12-09 17:00:00+00:00', 1800),
+(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', 'c8c74caf-55f8-45a7-bbb3-d0cfa2fb6d10', '2024-12-10 02:00:00+00:00', 1500),
+(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '19eae21d-cd9e-4cf7-9269-8a1adc9b416d', '2024-12-10 05:00:00+00:00', 1500),
+(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '347a0fc6-0541-44b4-aead-c34f7f783f10', '2024-12-10 08:00:00+00:00', 1800),
+(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', 'd349bc89-793e-4bed-a684-3bbc97162d51', '2024-12-10 11:00:00+00:00', 1800),
+(gen_random_uuid(), 'b7f76a6d-f8c3-404e-b20f-fce253237a28', '3bbb4a0d-c774-443a-b44b-0ad7695a658b', '2024-12-10 14:00:00+00:00', 1800),
 
 -- Hall E (Dec 10)
 (gen_random_uuid(), 'f826f9d7-a64b-4db5-ac9a-08e6e568fbe0', '19eae21d-cd9e-4cf7-9269-8a1adc9b416d', '2024-12-09 17:00:00+00:00', 1800),
